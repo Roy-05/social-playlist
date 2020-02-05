cf1382caf31dd8dc8748ffdd724e5d4548a34f67
Dexter Ma <made@oregonstate.edu>
2020-02-04 18:38:43 -0800

    Updated directory structure

---
+++ socialPlaylist/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from socialPlaylist.forms import RegForm, LoginForm
+import secrets
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+# db = SQLAlchemy(app)
+
+# Placed below to avoid circular import of app
+from socialPlaylist import routes

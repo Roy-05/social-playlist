7b379d751bfdc0374bdcf6b93899380611cf9413
Justin Nand <nandj@oregonstate.edu>
2020-02-05 19:48:25 -0800

    added user model and login auth

--- socialPlaylist/__init__.py
+++ socialPlaylist/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from socialPlaylist.forms import RegForm, LoginForm
 import secrets
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
+login = LoginManager(app)
 
 application = app
 
--- socialPlaylist/routes.py
+++ socialPlaylist/routes.py
@@ -1,10 +1,14 @@
 
 from flask import Flask, render_template, url_for, flash, redirect
 from socialPlaylist import app, LoginForm, RegForm  # , db
+from flask_login import current_user, login_user
+from socialPlaylist.models import User
+
 
 @app.route('/')
-def mainPage():
+def index():
     return render_template('home.html')
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -12,13 +16,21 @@ def signup():
     # Validate based on data required and parameters listed in RegForm()
     if form.validate_on_submit():
         flash('Thanks for signing up!', 'success')
-        return redirect(url_for('mainPage'))
+        return redirect(url_for('index'))
     return render_template('signup.html', title='Sign Up', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit():        
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))        
+        login_user(user)
         flash('Logged In!', 'success')
-        return redirect(url_for('mainPage'))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Login', form=form)

e333f01649ce965465724300d55dccc9c81f6af6
git-tyl <git-tyl@users.noreply.github.com>
2020-02-07 12:38:47 +0800

    Feature/login form (#5)
    * #nfr update forms
    
    * #nfr update login routes
    
    Co-authored-by: nandj-osu <49378950+nandj-osu@users.noreply.github.com>

--- socialPlaylist/routes.py
+++ socialPlaylist/routes.py
@@ -8,7 +8,6 @@ from socialPlaylist import app, db
 def index():
     return render_template('home.html')
 
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegForm()
@@ -18,18 +17,24 @@ def signup():
         return redirect(url_for('index'))
     return render_template('signup.html', title='Sign Up', form=form)
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():        
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))        
-        login_user(user)
-        flash('Logged In!', 'success')
-        return redirect(url_for('index'))
+    if request.method == 'POST':
+        return submitLogin(form)
+    elif request.method == 'GET':
+        return showLoginForm(form)
+
+def submitLogin(form):
+    user = User.query.filter_by(email=form.email.data).first()
+    if user is None or not user.check_password(form.password.data):
+        flash('Invalid username or password')
+        return redirect(url_for('login'))
+    login_user(user)
+    flash('Logged In!', 'success')
+    return redirect(url_for('index'))
+
+def showLoginForm(form):
     return render_template('login.html', title='Login', form=form)

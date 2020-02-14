dc0dfcfcb469f07be354b8e05679ba726006a560
tyl <tyl@tyl.com>
2020-02-15 05:51:07 +0800

    #nfr update routes

--- socialPlaylist/routes.py
+++ socialPlaylist/routes.py
@@ -5,11 +5,13 @@ from socialPlaylist.models import User, Playlists, Song
 from socialPlaylist import app, db, login as loginManager
 from socialPlaylist.helpers import submitLogin, showLoginForm, getPlaylists, addNewPlaylist
 
-
 @app.route('/')
 def index():
     return render_template('home.html')
 
+#LOGIN SECTION: SIGN UP , LOGIN , LOGOUT, UNAUTHORIZED
+
+#SIGN UP ROUTE with get and post
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegForm()
@@ -26,6 +28,7 @@ def signup():
         return redirect(url_for('index'))
     return render_template('signup.html', title='Sign Up', form=form)
 
+#Login ROUTE with get and post
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -36,38 +39,7 @@ def login():
     elif request.method == 'GET':
         return showLoginForm(form)
 
-@app.route('/playlist', methods =['GET', 'POST'])
-@login_required
-def playlist():
-    form = CreatePlaylistForm()
-    if(request.method == 'POST'):
-        return addNewPlaylist(form)
-
-    return render_template('playlist.html', form=form, user = current_user.username, playlists = getPlaylists())
-
-
-
-
-@app.route('/addsong', methods =['GET', 'POST'])
-@login_required
-def addsong():
-    form = AddSongForm()
-    # Validate based on data required and parameters listed in RegForm()
-    form.playlist_id.choices = [(p.id, p.playlist_name) for p in Playlists.query.filter_by(user_id=current_user.id)]    
-    if form.validate_on_submit():        
-        # Instantiate song
-        # Add song to session
-        # Commit song to database
-        song = Song(title=form.title.data, artist_firstname=form.artist_firstname.data, artist_lastname=form.artist_lastname.data)
-        # Need to link playlist ID
-        # song.playlist_id =
-        db.session.add(song)
-        db.session.commit()
-        flash(f'Song added to playlist!', 'success')
-        return redirect(url_for('index'))
-    return render_template('addsong.html', title='Add Song', form=form)
-
-
+#logout ROUTE with get and post
 @app.route('/logout')
 @login_required
 def logout():
@@ -80,3 +52,33 @@ def unauthorized():
     flash('Please login.')
     return redirect(url_for('login'))
 
+#PLAYLIST SECTION
+
+#playlist ROUTE
+@app.route('/playlist', methods =['GET', 'POST'])
+@login_required
+def playlist():
+    form = CreatePlaylistForm()
+    if(request.method == 'POST'):
+        return addNewPlaylist(form)
+    return render_template('playlist.html', form=form, user = current_user.username, playlists = getPlaylists())
+
+#add song ROUTE
+@app.route('/addsong', methods =['GET', 'POST'])
+@login_required
+def addsong():
+    form = AddSongForm()
+    # Validate based on data required and parameters listed in RegForm()
+    form.playlist_id.choices = [(p.id, p.playlist_name) for p in Playlists.query.filter_by(user_id=current_user.id)]
+    if form.validate_on_submit():
+        # Instantiate song
+        # Add song to session
+        # Commit song to database
+        song = Song(title=form.title.data, artist_firstname=form.artist_firstname.data, artist_lastname=form.artist_lastname.data)
+        # Need to link playlist ID
+        # song.playlist_id =
+        db.session.add(song)
+        db.session.commit()
+        flash(f'Song added to playlist!', 'success')
+        return redirect(url_for('index'))
+    return render_template('addsong.html', title='Add Song', form=form)
